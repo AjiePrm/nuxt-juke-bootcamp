00fa276f63f07e11c9e35e269e8e4cb1de0774f7
AJIE SALAHUDIN PRIMA <ajiee.salahudin@gmail.com>
2023-05-12 14:26:32 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,507 @@
+<template>
+   <!-- header part -->
+   <header>
+        <div class="container-header ">
+              <div class="header-left">
+                <h1 class="tutor">Tutori<span>al</span></h1>
+              </div>
+              <!-- Tambahkan ikon menu di sini -->
+              <span class="fa fa-bars menu-icon"></span>
+              <div class="header-right">
+                <a class="link" href="#">Home</a>
+                <a class="link" href="#">ingredients</a>
+                <a class="link" href="#">How to</a>
+            </div>
+          </div>
+        </header>
+
+          <div class="section">
+            <div class="container-main relative">
+                <div class="custom-shape-divider-top-1683786047">
+                            
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                        </svg>
+                  </div>
+
+                  <div class="absolute container w-full m-0 flex flex-row justify-center mt-10 inset-0">    
+                    <div class="opening mt-10 text-5xl">
+                        <h1>How to make your <br>Burger</h1>
+                    </div>
+                    <div class="burger-img m-10">
+                        <img class="w-50 image animate-bounce " src="https://burgerburger.co.nz/wp-content/uploads/2020/01/Chick.jpg" alt="burger-img" width="500" height="500">
+                    </div>
+                  </div>
+
+                  <div class="absolute inset-x-0 bottom-10 h-16 flex justify-center" style="bottom: 140px;">
+                        <button class="rounded-lg bg-red-400 px-10 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Let's go
+                        </button>
+                  </div>
+
+                    <div class="custom-shape-divider-bottom-1683787314">
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                        </svg>
+                    </div> 
+            </div>
+          </div>
+          
+          <div class="section">
+            <div class="container-ing">
+                <div class="mb-10 pm-10">
+                  <h1 class="title mb-10 pm-10">ingredients</h1>
+                </div>
+                <table class="container-table">
+                    <thead>
+                        <tr>
+                            <th><h1>Qty</h1></th>
+                            <th><h1>ingredient</h1></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1/2</td>
+                            <td>pound ground beef</td>
+                        </tr>
+
+                        <tr>
+                            <td>1</td>
+                            <td>Pinch of salt and pepper</td>
+                        </tr>
+
+                        <tr>
+                            <td>1</td>
+                            <td>tomato, sliced</td>
+                        </tr>
+
+                        <tr>
+                            <td>1/4</td>
+                            <td>onion, sliced</td>
+                        </tr>
+
+                        <tr>
+                            <td>1</td>
+                            <td>Lettuce leaves</td>
+                        </tr>
+
+                        <tr>
+                            <td>2</td>
+                            <td>hamburger buns</td>
+                        </tr>
+
+                        <tr>
+                            <td>optional</td>
+                            <td>Cheese</td>
+                        </tr>
+
+                        <tr>
+                            <td>optional</td>
+                            <td>Bacon</td>
+                        </tr>
+                        
+                        <tr>
+                            <td>optional</td>
+                            <td>Condiments (e.g., ketchup, mustard, mayonnaise)</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+          </div>
+
+          <div class="section">
+            <div class="container-ing w-fit">
+                <div>
+                    <h1 class="title">How to make</h1>
+                </div>
+                <div class="list text-2xl ">
+                    <ul>
+                        <li>Start by preheating a skillet or grill over medium heat.</li>
+                        <li>Form ground beef into a patty about 4-5 inches wide and 1/2 inch thick.</li>
+                        <li>Season both sides of the patty with salt and pepper.</li>
+                        <li>Place the patty on the skillet or grill and cook for about 4-5 minutes on each side, or until the internal temperature reaches 160°F (71°C).</li>
+                        <li>While the patty is cooking, slice a tomato, onion, and lettuce.</li>
+                        <li>Toast the burger bun on the skillet or grill for about 1-2 minutes, or until lightly browned.</li>
+                        <li>Assemble the burger by placing the lettuce, tomato, and onion on the bottom half of the bun, followed by the cooked patty.</li>
+                        <li>Add your favorite toppings, such as cheese, bacon, or condiments, on top of the patty.</li>
+                        <li>Finish by placing the top half of the bun on the burger.</li>
+                        <li>Enjoy!</li>
+                      </ul>
+                </div>
+            </div>
+          </div>
+
+          <div class="video-burger">
+            <iframe class="aspect-video" width="560" height="315" src="https://www.youtube.com/embed/KiHBmYx_G9k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <svg viewBox="0 0 1887 200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgba(145, 50, 50, 1)" d="M 0 0 C 954 0 0 110 954 110 L 954 110 L 954 0 L 0 0 Z" stroke-width="0"></path> <path fill="rgba(145, 50, 50, 1)" d="M 953 110 C 1903 110 953 183 1903 183 L 1903 183 L 1903 0 L 953 0 Z" stroke-width="0"></path> </svg>
+        </div>
+      
+</template>
+
+<style>
+* {
+    box-sizing: border-box;
+  }
+  
+  body {
+    margin: 0;
+    font-family: "Poppins";
+    background-size: cover;
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.com/svgjs' width='1440' height='1000' preserveAspectRatio='none' viewBox='0 0 1440 1000'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1012%26quot%3b)' fill='none'%3e%3crect width='1440' height='1000' x='0' y='0' fill='rgba(235%2c 224%2c 206%2c 1)'%3e%3c/rect%3e%3cpath d='M134.43 863.96 a298.81 298.81 0 1 0 597.62 0 a298.81 298.81 0 1 0 -597.62 0z' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float2'%3e%3c/path%3e%3cpath d='M-88.42 997.22 a258.85 258.85 0 1 0 517.7 0 a258.85 258.85 0 1 0 -517.7 0z' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float1'%3e%3c/path%3e%3cpath d='M933.9473443860645 345.2529160792659L709.3730658298939 309.68384465886027 898.3782729656589 569.8271946354365z' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float2'%3e%3c/path%3e%3cpath d='M1260.888%2c194.678C1303.849%2c194.529%2c1344.122%2c171.901%2c1364.868%2c134.281C1384.962%2c97.843%2c1377.279%2c55.036%2c1358.322%2c17.994C1337.074%2c-23.525%2c1307.528%2c-68.684%2c1260.888%2c-68.891C1214.049%2c-69.099%2c1183.425%2c-24.5%2c1161.943%2c17.122C1142.6%2c54.6%2c1135.124%2c98.164%2c1155.654%2c135.005C1176.693%2c172.759%2c1217.668%2c194.828%2c1260.888%2c194.678' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float1'%3e%3c/path%3e%3cpath d='M1444.226%2c982.5C1556.542%2c991.372%2c1663.694%2c927.602%2c1720.393%2c830.243C1777.45%2c732.268%2c1779.747%2c607.801%2c1717.704%2c512.905C1660.428%2c425.3%2c1548.891%2c407.252%2c1444.226%2c407.837C1340.909%2c408.415%2c1225.509%2c425.253%2c1175.994%2c515.934C1127.699%2c604.382%2c1184.309%2c704.498%2c1234.536%2c791.863C1284.999%2c879.638%2c1343.293%2c974.527%2c1444.226%2c982.5' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float3'%3e%3c/path%3e%3cpath d='M201.94 607.71 a168.17 168.17 0 1 0 336.34 0 a168.17 168.17 0 1 0 -336.34 0z' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float3'%3e%3c/path%3e%3cpath d='M457.99539173811075 785.0187304343312L687.06853613296 760.9421727728068 433.9188340765863 555.9455860394819z' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float1'%3e%3c/path%3e%3cpath d='M1277.2%2c884.653C1344.577%2c886.989%2c1408.022%2c846.317%2c1438.03%2c785.947C1465.403%2c730.878%2c1441.075%2c669.489%2c1410.743%2c615.992C1379.837%2c561.483%2c1339.859%2c501.848%2c1277.2%2c502.288C1214.972%2c502.725%2c1175.669%2c562.681%2c1146.868%2c617.845C1120.734%2c667.901%2c1109.811%2c724.687%2c1134.596%2c775.425C1162.764%2c833.089%2c1213.063%2c882.429%2c1277.2%2c884.653' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float3'%3e%3c/path%3e%3cpath d='M253.13 229.96 a288.6 288.6 0 1 0 577.2 0 a288.6 288.6 0 1 0 -577.2 0z' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float2'%3e%3c/path%3e%3cpath d='M35.67356877117258 769.77980479277L151.8668161486211 665.158934934016-68.94730108758152 653.5865574153215z' fill='rgba(255%2c 0%2c 0%2c 0.38)' class='triangle-float1'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1012'%3e%3crect width='1440' height='1000' fill='white'%3e%3c/rect%3e%3c/mask%3e%3cstyle%3e %40keyframes float1 %7b 0%25%7btransform: translate(0%2c 0)%7d 50%25%7btransform: translate(-10px%2c 0)%7d 100%25%7btransform: translate(0%2c 0)%7d %7d .triangle-float1 %7b animation: float1 5s infinite%3b %7d %40keyframes float2 %7b 0%25%7btransform: translate(0%2c 0)%7d 50%25%7btransform: translate(-5px%2c -5px)%7d 100%25%7btransform: translate(0%2c 0)%7d %7d .triangle-float2 %7b animation: float2 4s infinite%3b %7d %40keyframes float3 %7b 0%25%7btransform: translate(0%2c 0)%7d 50%25%7btransform: translate(0%2c -10px)%7d 100%25%7btransform: translate(0%2c 0)%7d %7d .triangle-float3 %7b animation: float3 6s infinite%3b %7d %3c/style%3e%3c/defs%3e%3c/svg%3e");
+  }
+  
+
+  /* style header */
+  a {
+    text-decoration: none;
+  }
+  
+  .clear {
+    clear: left;
+  }
+  
+  .container-header {
+    max-width: 1170px;
+    width: 100%;
+    padding: 0 15px;
+    margin: 0 auto;
+  }
+  
+  header {
+    height: 65px;
+    width: 100%;
+    background-color: #F5B614;
+    position :fixed;
+    top: 0; 
+    z-index: 10;
+    opacity: 0.7;
+  }
+  
+  .tutor {
+    width: 90px;
+    margin-top: 10px;
+    font-size: 32px;
+  }
+
+  .tutor span{
+    color: #da4525;
+  }
+  
+  .header-left {
+    float: left;
+  }
+  
+  .header-right {
+    float: right;
+    margin-right: 40px;
+  }
+  
+  .link{
+    line-height: 65px;
+    font-size: 24px;
+    padding: 0 30px;
+    color: white;
+    float: left;
+    opacity: 0.5;
+    transition: all 0.5s;
+    text-decoration: none;
+    
+ }
+  
+  .link:hover {
+    color: #000000 ;
+    padding: 0 30px;
+    opacity: 0.3px;
+    font-size: 20px;
+    text-decoration: underline #da4525;
+    transition: 0.5s ease;
+    opacity: 1;
+
+ }
+ 
+ .btn-cart img {
+    width: 50px;
+    opacity: 0.5;
+    margin-top: 20px;
+    padding-left: 30px;
+  }
+
+  .btn-cart img:hover {
+    filter: brightness(50%);
+    transform: scale(1.1);
+    opacity: 1;
+  }
+
+  /* CSS untuk menu-icon */
+  .menu-icon{
+    color: #F5B614;
+    float: right;
+    font-size: 50px;
+    padding: 10px 0;
+    /* display ke none */
+    display:none;
+    margin-right: 5px;
+  }
+  /* style header */
+  
+  /* style table */
+  .container-ing{
+    width: 1216px;
+    height: 781px;
+    background: #F5B614;
+    border-radius: 20px;
+    position: relative;
+    max-width: 100%;
+  }
+   .title{
+    font-size:3em; 
+    font-weight: 300;
+    line-height:1em;
+    text-align: center;
+    color: white;
+    margin: 60px 40px;
+   }
+
+   .container-table th h1{ 
+    font-weight: bold;
+	  font-size: 1em;
+    text-align: left;
+    color: white;
+   }
+
+   .container-table td{
+    font-weight: normal;
+	  font-size: 1em;
+    color: white;
+    flex-direction: column;
+    -webkit-box-shadow: 0 2px 2px -2px #0E1119;
+	   -moz-box-shadow: 0 2px 2px -2px #0E1119;
+	        box-shadow: 0 2px 2px -2px #0E1119; 
+   }
+
+   .container-table{
+    text-align: left;
+	  overflow: hidden;
+	  width: 80%;
+	  margin: 0 auto;
+    display: table;
+    padding: 0 0 8em 0;
+   }
+
+   .container-table td, .container-table th {
+	  padding-bottom: 1%;
+	  padding-top: 2%;
+    padding-left:2%;  
+}
+  
+    .container-table tr:nth-child(odd) {
+      background-color: #323C50;
+    }
+
+    .container-table tr:nth-child(even) {
+      background-color: #2C3446;
+  }
+
+  .container-table tr:hover {
+    background-color: #464A52;
+    -webkit-box-shadow: 0 6px 6px -6px #0E1119;
+      -moz-box-shadow: 0 6px 6px -6px #0E1119;
+           box-shadow: 0 6px 6px -6px #0E1119;
+ }
+
+ .container-table td:hover {
+  background-color: #FFF842;
+  color: #403E10;
+  font-weight: bold;
+  
+  box-shadow: #7F7C21 -1px 1px, #7F7C21 -2px 2px, #7F7C21 -3px 3px, #7F7C21 -4px 4px, #7F7C21 -5px 5px, #7F7C21 -6px 6px;
+  transform: translate3d(6px, -6px, 0);
+  
+  transition-delay: 0s;
+	  transition-duration: 0.4s;
+	  transition-property: all;
+  transition-timing-function: line;
+}
+/* style table */
+
+/* ------------------------------------------------------ */
+
+/* style bagian tutor */
+
+.list ul{
+  list-style-type: none;
+  padding-left: 50px;
+}
+
+.list li{
+  margin-bottom: 10px;
+  position: relative;
+  transition: all 0.5s;
+}
+
+.list li:hover{
+  color: white;
+  font-weight: bold;
+}
+
+.list li:before {
+  content: "•";
+  position: absolute;
+  left: -20px;
+  top: 2px;
+  color: #ff6f00;
+  font-weight: bold;
+}
+
+.video-burger{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 100px;
+  flex-direction: column;
+}
+
+/* ------------------------------------------------------ */
+
+.custom-shape-divider-top-1683786047 {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+}
+
+.custom-shape-divider-top-1683786047 svg {
+  position: relative;
+  display: block;
+  width: calc(100% + 1.3px);
+  height: 150px;
+}
+
+.custom-shape-divider-top-1683786047 .shape-fill {
+  fill: #FFFFFF;
+}
+
+.custom-shape-divider-bottom-1683787314 {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+  transform: rotate(180deg);
+}
+
+.custom-shape-divider-bottom-1683787314 svg {
+  position: relative;
+  display: block;
+  width: calc(100% + 1.3px);
+  height: 79px;
+  transform: rotateY(180deg);
+}
+
+.custom-shape-divider-bottom-1683787314 .shape-fill {
+  fill: #FFFFFF;
+}
+
+/* responsive header */
+
+/* Untuk Tablet */
+@media screen and (max-width: 1000px){
+  .header-right a{
+    font-size: 14px;
+    padding: 10px;
+  }
+
+  .link:hover {
+    color: #000000 ;
+    padding-top: 30px;
+    padding: 0 20px;
+    opacity: 0.3px;
+    font-size: 14px;
+    text-decoration: underline #da4525;
+    transition: 0.5s ease;
+    opacity: 1;
+}
+
+  .container-header span{
+    font-size: 32px;
+  }
+
+  .btn-cart img{
+    width: auto;
+    padding-left: 20px;
+  }
+
+  .header-right{
+    margin-right: 10px;
+  }
+
+  .link{
+    line-height: 45px;
+  }
+
+  .opening h1{
+    font-size: 28px;
+  }
+}
+  /* Untuk Smartphone */
+  @media screen and (max-width: 670px) {
+    .header-right{
+      display:none;
+    }
+    .menu-icon{
+      color: #ad5425;
+      font-size: 32px;
+      line-height: 30px;
+    }
+
+    .container-ing{
+      height: 1000px;
+  }
+
+  .container-table td, .container-table th {
+	  padding-bottom: 3%;
+	  padding-top: 3%;
+    padding-left:2%;  
+  }
+  .container-table{
+    margin-top: 30px;
+  }
+  }
+/* ------------------------------------------------------ */
+
+
+/* bagian main */
+.section{
+  display: flex;
+  margin: 100px auto;
+  align-items: center;
+  justify-content: center;
+}
+
+.container-main{
+  width: 1216px;
+  height: 781px;
+  background: #F5B614;
+  border-radius: 20px;
+  max-width: 100%;
+  display: flex;
+  flex-direction: row;
+
+}
+
+.opening h1{
+ color: white;
+ margin-top: 220px;
+}
+
+.image{
+  margin-top: 80px;
+  clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 75% 75%, 75% 100%, 50% 75%, 0% 75%);
+  border-radius: 10px;
+}
+
+/* ------------------------------------------------------ */
+</style>
